9db336d3bd3dc4a1f4b9e1d09f7e423fa5395f9a
Daniel John <dj71286@gmail.com>
2023-02-20 23:45:57 +0000

    Tiny fixes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -36,7 +36,7 @@ void bubble_sort(int *array, size_t size)
 				 * array[i] = array[i + 1];
 				 * array[i + 1] = temp;
 				 */
-				swap(&array[i], &array[i + 1]);
+				swap(array + i, array + i + 1);
 				print_array(array, size);
 				bubbly = false;
 			}
